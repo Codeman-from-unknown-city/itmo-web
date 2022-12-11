453edfc66230b161b4ec716714fed8c38719232b
Трущёв Сергей <i_truschev@mail.ru>
2022-12-11 17:53:51 +0300

    add adaptive design

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector('.header__burger').addEventListener(
+    'click',
+    () => document.querySelector('header')
+                  .classList
+                  .toggle('menu_active')
+)
